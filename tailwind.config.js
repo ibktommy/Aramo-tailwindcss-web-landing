2b7ae9f4deb987c825e512e35dfd809819c00e26
ibktommy <atomquartz@gmail.com>
2022-04-23 17:33:29 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			colors: {
+				colorWhite: "#fff",
+				colorDark: "#1A250E",
+				pryColorLight: "#F3F8EC",
+				pryColorDark: "#669135",
+			},
+		},
+	},
+	plugins: [],
+};
